20ea5eabf029e48752da42563cc8ce57d11f8470
mattiwos <bmattiwos@gmail.com>
2021-12-18 19:28:04 +0000

    inital website for wos

---
+++ wossite_v1/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/pages/Home/Home'
+// import Routing from './Components/Routing/Routing'
+
+function App() {
+  return (
+    <Home/>
+    // <div className="App"> //Set theme of website
+    //       <Routing/>
+
+    //   <header className="App-header">//Nav for website  
+    //     //Nav  
+    //   </header>
+    //   //Routing 
+
+    // </div>
+  );
+}
+
+export default App;
